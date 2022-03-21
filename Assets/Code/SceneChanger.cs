ce4b8a1e42a614082daca0bde023ab95ef7ff8ae
Jari Pimiä <jari.pimia@tuni.fi>
2022-03-21 16:55:50 +0200

    Basic MainMenu lisätty

--- Assets/Code/SceneChanger.cs
+++ Assets/Code/SceneChanger.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 namespace TeatterinMysteeri
 {
-    public class SceneChanger : MonoBehaviour
+    public class SceneChanger : MonoBehaviour, IPointerDownHandler
     {
         [SerializeField]
         private string sceneName;
         private void OnTriggerEnter2D(Collider2D other)
+        {
+            LevelLoader.Current.LoadLevel(sceneName);
+        }
+        public void OnPointerDown(PointerEventData eventData)
         {
             LevelLoader.Current.LoadLevel(sceneName);
         }
