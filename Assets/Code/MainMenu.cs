5d78d36d68225074d45dd4faf99bc4c8c241ca73
Jari Pimiä <jari.pimia@tuni.fi>
2022-04-25 15:27:31 +0300

    exit game nabbula tekee asioita

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -26,5 +26,10 @@ namespace TeatterinMysteeri
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
         }
+        public void ExitApplication()
+        {
+            Debug.Log("neekeri");
+            Application.Quit();
+        }
     }
 }
